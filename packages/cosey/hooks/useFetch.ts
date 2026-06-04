04cff18a6710036d678dae6fa5fda98a6bd30d3a
wuzhitao <joinpass@outlook.com>
2026-06-04 16:36:14 +0800

    feat: 允许获取 useFetch 的promise对象

--- packages/cosey/hooks/useFetch.ts
+++ packages/cosey/hooks/useFetch.ts
@@ -14,6 +14,7 @@ interface UseFetchStale<T> {
   isFetching: ShallowRef<boolean>;
   data: ShallowRef<T | undefined>;
   error: ShallowRef<any>;
+  promise: ShallowRef<Promise<T>>;
 }
 
 interface UseFetchResult<T, U> extends UseFetchStale<T> {
@@ -23,15 +24,18 @@ interface UseFetchResult<T, U> extends UseFetchStale<T> {
 const staleMap = new Map<any, UseFetchStale<any>>();
 
 export function useFetch<T = any, U = any>(
-  fetcher: (params: U) => Promise<any> | any,
+  fetcher: (params: U) => Promise<T> | T,
   options: UseFetchOptions<T> = {},
 ): UseFetchResult<T, U> {
   const { immediate = true, initialData, stale, onSuccess, onError, onFinally } = options;
 
-  const { isFetching, error, data } = (!isNullish(stale) && staleMap.get(stale)) || {
+  const { promise: firstPromise, resolve, reject } = Promise.withResolvers<T>();
+
+  const { isFetching, error, data, promise } = (!isNullish(stale) && staleMap.get(stale)) || {
     isFetching: shallowRef(false),
     data: shallowRef<T | undefined>(initialData),
     error: shallowRef<any>(),
+    promise: shallowRef<Promise<T>>(firstPromise),
   };
 
   if (!isNullish(stale) && !staleMap.get(stale)) {
@@ -39,8 +43,13 @@ export function useFetch<T = any, U = any>(
       isFetching,
       error,
       data,
+      promise,
     });
   }
+
+  const promisedFetcher = async (params: U) => {
+    return await fetcher(params); // 自动处理 Promise 和普通值
+  };
 
   async function execute(params?: U) {
     if (isFetching.value) {
@@ -51,15 +60,19 @@ export function useFetch<T = any, U = any>(
       error.value = undefined;
       isFetching.value = true;
 
-      const res = await fetcher(params as U);
+      promise.value = promisedFetcher(params as U);
+
+      const res = await promise.value;
       data.value = res;
       onSuccess?.(res);
+      resolve(res);
     } catch (err) {
       error.value = err;
       onError?.(err);
     } finally {
       isFetching.value = false;
       onFinally?.();
+      reject();
     }
   }
 
@@ -76,5 +89,6 @@ export function useFetch<T = any, U = any>(
     error,
     data,
     execute,
+    promise,
   };
 }
